e249800353bc890deb98e035c6514b77320ceabe
chuckrapp <chuck.r.rapp>
2017-08-27 21:45:41 -0400

    added status, some more CSS, and some error catching

--- javascript/logic.js
+++ javascript/logic.js
@@ -20,12 +20,35 @@ var assign = function() {
   }
 }
 
-assign()
+// var checkTurn = function() {
+//   if (turn == 1) {
+//     $("#status").text("Red's Turn!");
+//   } else if (turn == 2) {
+//     $("#status").text("Blue's Turn!");
+//   }
+// }
+
+var clearCards = function() {
+  card1Val = "";
+  card2Val = "";
+}
 
 //check cards for a match 
 //if correct player goes again, otherwise play passes
 var checkMatch = function() {
+  console.log(card1Val);
+  console.log(card2Val);
   if(card1Val == card2Val) {
+    lockCard();
+    if (turn == 1) {
+      $("#status").text('Correct! Red, you go again!');
+      $("#" + card1Id + "").addClass("red");
+      $("#" + card2Id + "").addClass("red");
+    } else if (turn == 2) {
+      $("#status").text('Correct! Blue, you go again!');
+      $("#" + card1Id + "").addClass("blue");
+      $("#" + card2Id + "").addClass("blue");
+    }
     console.log("Correct! You go again!");
     if(turn == 1) {
       scoreRed++;
@@ -35,37 +58,60 @@ var checkMatch = function() {
       $("#blueScore").text(scoreBlue);
     }
   } else {
+    if (turn == 1) {
+      $("#status").text("Sorry, no match! Blue's turn!");
+    } else if (turn == 2) {
+      $("#status").text("Sorry, no match! Red's turn!");
+    }
     console.log("No match! Next players turn");
-      setTimeout(function(){ flipBack() } , 1000);
+    //keep cards flipped over for 1 second so players can see values
+      setTimeout(function(){ flipBack() } , 1500);
     if(turn == 1) {
       turn++;
+      // redTurn();
     } else {
       turn = 1;
+      // blueTurn();
     }
   }
+  clearCards();
+}
+
+//lock cards if correct to avoid double clicks
+var lockCard = function() {
+  console.log("locking " + card1Id);
+  console.log("locking " + card2Id);
+  $("#" + card1Id + "").removeClass("card");
+  $("#" + card2Id + "").removeClass("card");
 }
 
 //flip both cards back over
 var flipBack = function() {
-  $("#" + card1Id + "").children('h2').text("0");
-  $("#" + card2Id + "").children('h2').text("0");
+  $("#" + card1Id + "").children('h2').text("");
+  $("#" + card2Id + "").children('h2').text("");
+    // checkTurn();
+
 }
+
+assign();
+// checkTurn();
 
 //reveal value of card when a it is clicked
 //TODO: rewrite this into function - repeat code 
 $(".card").on("click", function() {
   if (flipped == 0) {
-    $(this).children('h2').text($(this).data('value'));
     card1Val = $(this).data('value');
     card1Id = this.id;
+    $(this).children('h2').text($(this).data('value'));
     flipped++;
   }  else if (flipped == 1) {
-    $(this).children('h2').text($(this).data('value'));
     card2Val = $(this).data('value');
     card2Id = this.id;
+    $(this).children('h2').text($(this).data('value'));
     checkMatch();
     flipped = 0;
   }
+
 });
 
 console.log(arrOptions);
