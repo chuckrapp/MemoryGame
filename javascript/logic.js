520deb0e8700e9878559ce20234658a8753b522f
chuckrapp <chuck.r.rapp>
2017-09-06 22:21:47 -0400

    added a lot of error handling - almost all logic is complete

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,6 +1,6 @@
 var arrOptions = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8"] //all of the possible options
-var correctRed = [];
-var correctBlue = [];
+var correctRed = 0;
+var correctBlue = 0;
 var flipped = 0; //counts how many cards are currently flipped over
 var turn = 1; //turn 1 = RED, turn 2 = BLUE
 var scoreRed = 0; 
@@ -10,15 +10,84 @@ var card2Val; //value of the second card flipped
 var card1Id; //position of the fist card flipped
 var card2Id; //position of the second card flipped
 
-//shuffle the array of options
-arrOptions.sort(function() { return 0.5 - Math.random() });
+
 
 //apply array contents to cards
-var assign = function() {
+var startGame = function() {
+  //shuffle the array of options
+  // arrOptions.sort(function() { return 0.5 - Math.random() });
+  console.log(arrOptions);
+
+  correctBlue = 0;
+  correctRed = 0;
+
+
   for (var i = 0; i < arrOptions.length; i++) {
     $("#card"+[i]+"").data("value", arrOptions[i]);
+    $("#card"+[i]+"").children('h2').text("");
+  }
+
+  if (!$(".card").hasClass("card-active")){
+    $(".card").addClass('card-active');
+    $(".card").removeClass('red blue');
   }
 }
+
+//reveal value of card when a it is clicked
+//TODO: rewrite this into function - repeat code 
+var enableCards = function() {
+  console.log('ENABLING all cards')
+  $(".card-active").on("click", function() {
+  if (flipped == 0) {
+    card1Val = $(this).data('value');
+    card1Id = this.id;
+    lockCard(card1Id);
+    $(this).children('h2').text($(this).data('value'));
+    flipped++;
+  }  else if (flipped == 1) {
+    disableCards();
+
+    card2Val = $(this).data('value');
+    card2Id = this.id;
+    lockCard(card2Id);
+
+    $(this).children('h2').text($(this).data('value'));
+    checkMatch();
+    flipped = 0;
+  }
+});
+}
+
+//flip both cards back over
+var flipBack = function() {
+  $("#" + card1Id + "").children('h2').text("");
+  $("#" + card2Id + "").children('h2').text("");
+  // enableCards();
+}
+
+//lock cards if correct to avoid double clicks
+//ToDo -- LockCard is not working? class being removed but cards still clickable????
+// var lockCard = function() {
+//   console.log("locking " + card1Id);
+//   console.log("locking " + card2Id);
+//   $("#" + card1Id + "").removeClass('card-active');
+//   $("#" + card2Id + "").removeClass('card-active');
+  
+//   $("#" + card1Id + "").off('click');
+//   $("#" + card2Id + "").off('click');
+// }
+
+var lockCard = function(id) {
+  console.log("locking " + id);
+  $("#" + id + "").off('click');
+}
+
+var disableCards = function() {
+  console.log('disabling all cards')
+  $(".card").off('click');
+}
+
+
 
 var clearCards = function() {
   card1Val = "";
@@ -27,14 +96,29 @@ var clearCards = function() {
 
 //Check for winner
 var checkWin = function() {
-  if (correctRed.length + correctBlue.length == arrOptions.length) {
-    if (correctRed.length > correctBlue.length) {
+  if (correctRed + correctBlue == (arrOptions.length/2)) {
+    if (correctRed > correctBlue) {
       $("#status").text("Red Wins!!");
-    } else {
+      scoreRed++;
+      $("#redScore").text(scoreRed);
+    } else if ( correctRed < correctBlue) {
       $("#status").text("Blue Wins!!");
+      scoreBlue++;
+      $("#blueScore").text(scoreBlue);
+    } else {
+      $("#status").text("Wow! Tie Game!!");
     }
+  setTimeout(startGame(),1500);
   }
 }
+
+
+
+if(turn == 1) {
+      
+    } else {
+      
+    } 
 
 //check cards for a match 
 //if correct player goes again, otherwise play passes
@@ -42,27 +126,24 @@ var checkMatch = function() {
   console.log(card1Val);
   console.log(card2Val);
   if(card1Val == card2Val) {
-    lockCard();
+    $("#" + card1Id + "").removeClass('card-active');
+    $("#" + card2Id + "").removeClass('card-active');
+
     if (turn == 1) {
       $("#status").text('Correct! Red, you go again!');
       $("#" + card1Id + "").addClass("red");
       $("#" + card2Id + "").addClass("red");
-      correctRed.push(card1Id, card2Id);
+      correctRed++;
     } else if (turn == 2) {
       $("#status").text('Correct! Blue, you go again!');
       $("#" + card1Id + "").addClass("blue");
       $("#" + card2Id + "").addClass("blue");
-      correctBlue.push(card1Id, card2Id);
+      correctBlue++;
     }
     console.log("Correct! You go again!");
-    if(turn == 1) {
-      scoreRed++;
-      $("#redScore").text(scoreRed);
-    } else {
-      scoreBlue++;
-      $("#blueScore").text(scoreBlue);
-    } 
+    
     checkWin();
+    enableCards();
   } else {
     if (turn == 1) {
       $("#status").text("Sorry, no match! Blue's turn!");
@@ -72,50 +153,15 @@ var checkMatch = function() {
     console.log("No match! Next players turn");
     //keep cards flipped over for 1 second so players can see values
       setTimeout(function(){ flipBack() } , 1500);
+      setTimeout(function(){ enableCards() } , 1500);
     if(turn == 1) {
       turn++;
-      // redTurn();
     } else {
       turn = 1;
-      // blueTurn();
     }
   }
   clearCards();
 }
 
-//lock cards if correct to avoid double clicks
-//ToDo -- LockCard is not working? class being removed but cards still clickable????
-var lockCard = function() {
-  console.log("locking " + card1Id);
-  console.log("locking " + card2Id);
-  $("#" + card1Id + "").removeClass("card");
-  $("#" + card2Id + "").removeClass("card");
-}
-
-//flip both cards back over
-var flipBack = function() {
-  $("#" + card1Id + "").children('h2').text("");
-  $("#" + card2Id + "").children('h2').text("");
-}
-
-assign();
-
-//reveal value of card when a it is clicked
-//TODO: rewrite this into function - repeat code 
-$(".card").on("click", function() {
-  if (flipped == 0) {
-    card1Val = $(this).data('value');
-    card1Id = this.id;
-    $(this).children('h2').text($(this).data('value'));
-    flipped++;
-  }  else if (flipped == 1) {
-    card2Val = $(this).data('value');
-    card2Id = this.id;
-    $(this).children('h2').text($(this).data('value'));
-    checkMatch();
-    flipped = 0;
-  }
-});
-
-console.log(arrOptions);
-
+startGame();
+enableCards();
